2eec64148391aca5c853c681276eb5f9ac3c8354
Gabriel Ernesto Manuel <gernestome@hotmail.com>
2019-02-13 15:24:09 -0600

    Revert "laboratorio2 done"

--- src/app/songs.component.ts
+++ src/app/songs.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
     template: `
         <div>
             <h2>this is the list of my favourites songs </h2>
-            <input type="text" name="song" [(ngModel)]="title">
-            <button (click)="addSong()">Agregar una cancion</button>
             <ul>
                 <li *ngFor="let song of songs">{{song}}</li>
             </ul>
@@ -13,22 +11,9 @@ import { Component } from '@angular/core';
     selector: 'songs'
 })
 export class SongsComponent {
-
-    public title:string = null;
-
     songs = [
         'Nobody knows when you are out and down',
         'Layla',
         'Drive'
     ];
-
-    addSong() {
-        console.log('add song');
-        // mutating
-        //this.songs.push(this.title);
-        //using spread operator to avoid mutations
-        this.songs = [...this.songs, this.title];
-        this.title = null;
-    }
-
 }
